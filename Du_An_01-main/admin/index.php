709f286476e839a2ebf8eac8dfe4f051580b52c3
Hoanguy01 <bouyno123@gmail.com>
2024-11-23 15:25:43 +0700

    first commit

---
+++ Du_An_01-main/admin/index.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'controller/studentController.php';
+include_once 'model/student.php';
+include_once 'model/studentQuery.php';
+
---
+++ Du_An_01-main/common/env.php
@@ -0,0 +1,9 @@
+<?php
+defined('DB_HOST', "localhost");
+defined('DB_PORT', "3306");
+defined('DB_HOST', "localhoat");
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'students');
+
+define('BASE_URL', 'http://localhost/ca2-xth-thu-cung/');
